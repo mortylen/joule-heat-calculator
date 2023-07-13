acba76faf9e568a6c6681a1f8132112a7920d533
mortylen <123174309+mortylen@users.noreply.github.com>
2023-07-13 16:35:08 +0000

    add get_spec_heat_from_vec to main.rs

--- src/main.rs
+++ src/main.rs
@@ -34,25 +34,59 @@ fn main() {
     
     println!("{:#?}", tbl_spec_heat);
     
-    get_down(&tbl_spec_heat);
-    get_up(&tbl_spec_heat);
+    //get_down(&tbl_spec_heat);
+    //get_up(&tbl_spec_heat);
+
+    println!("{:#?}", get_spec_heat_from_vec(&tbl_spec_heat, 0.9));
+    //get_spec_heat_from_vec(&tbl_spec_heat, 6.0);
 }
 
-fn get_down(tbl_data: &Vec<SpecHeat>) {
-    let down_number: Vec<&SpecHeat> = tbl_data
-        .into_iter()
-        .filter(|n| n.temperature <= 19.0)
-        .collect();
+fn get_spec_heat_from_vec(tbl_data: &Vec<SpecHeat>, temperature: f64) -> Option<f64> {
+    let (down_temp, down_heat) = match tbl_data.iter().find(|&x| x.temperature <= temperature) {
+        Some(spec_heat) => (spec_heat.temperature, spec_heat.specific_heat),
+        None => (tbl_data.first()?.temperature, tbl_data.first()?.specific_heat),
+        //None => return None,
+    };
+
+    let (up_temp, up_heat) = match tbl_data.iter().find(|&x| x.temperature >= temperature) {
+        Some(spec_heat) => (spec_heat.temperature, spec_heat.specific_heat),
+        None => (tbl_data.last()?.temperature, tbl_data.last()?.specific_heat),
+        //None => return None,
+    };
+
     
-    println!("{:#?}", down_number.last());
+    let delta_temperature = if up_temp == down_temp {
+        down_temp
+    } else {
+        up_temp - down_temp
+    };
+
+    let delta_specifi_heat = if up_heat == down_heat {
+        down_heat
+    } else {
+        up_heat - down_heat
+    };
+
+    let res = ((temperature - down_temp) / delta_temperature) * delta_specifi_heat + down_heat;
+
+    Some(res)
 }
 
-fn get_up(tbl_data: &Vec<SpecHeat>) {
-    let up_number: Vec<&SpecHeat> = tbl_data
-        .into_iter()
-        .filter(|n| n.temperature >= 2.0)
-        .collect();
-    
-    println!("{:#?}", up_number.first());
-}
+
+
+//fn get_down(tbl_data: &Vec<SpecHeat>) {
+//    let down_number: Vec<&SpecHeat> = tbl_data
+//        .into_iter()
+//        .filter(|n| n.temperature <= 19.0)
+//        .collect();   
+//    println!("{:#?}", down_number.last());
+//}
+
+//fn get_up(tbl_data: &Vec<SpecHeat>) {
+//    let up_number: Vec<&SpecHeat> = tbl_data
+//        .into_iter()
+//        .filter(|n| n.temperature >= 2.0)
+//        .collect();   
+//    println!("{:#?}", up_number.first());
+//}
  
